b8910f795bdd2a920aab22f4a8a711c428a81bd7
Sophie Xhonneux <lpxhonneux@gmail.com>
2024-09-04 09:30:42 -0400

    Changing some keybindings

--- lua/sophiex/lazy/harpoon.lua
+++ lua/sophiex/lazy/harpoon.lua
@@ -13,9 +13,9 @@ return {
         vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-m>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-,>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-.>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
